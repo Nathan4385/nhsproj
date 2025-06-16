7b7ae6321142a5feabea81b05f5043045f1b3141
Nathan4385 <murphynathan2005@gmail.com>
2025-06-16 11:30:17 +0100

    Initial commit

--- models/user.js
+++ models/user.js
@@ -1,16 +1,8 @@
-// models/User.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
